3caef9b17692ac452b5f7200c103daeb003b5d20
Whoopsunix <whoopsunix@whoopsunix.com>
2024-02-24 19:33:10 +0800

    sync :)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://whoopsunix.com',
+    url: 'https://whoopsunix.com/',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
